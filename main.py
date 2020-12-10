2eb05814d2caddc5a5708d5d6a07f1dff42f37de
Rohan Ghosh <rohanghosh9093@gmail.com>
2020-12-10 22:41:13 +0530

    message function added

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import discord
 from discord import client
 from discord.ext import commands
 
+botToken = 'NzI5MjM3NzU0Mzk0OTY4MTA0.XwGBlQ.VDFy_t6JbOa8CGV3Am9lK45nFb8'
 client = commands.Bot(command_prefix="!")
 
 
 @client.event
-async def on_login():
+async def on_ready():
     print("The bot has been deployed")
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hi!")
+
+client.run(botToken)
