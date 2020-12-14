7efa5e8efdd76e11c09b7917c57f8c978b461b0b
Rohan Ghosh <rohanghosh9093@gmail.com>
2020-12-14 22:15:43 +0530

    User Mention Added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
-from datetime import datetime, time
+from datetime import datetime
 from asyncio import sleep
 
 
-botToken = ''
+botToken = open("botToken.txt", "r")
+botToken = botToken.read()
 client = commands.Bot(command_prefix="!bot ")
 
 
@@ -18,20 +19,19 @@ async def hello(ctx):
     await ctx.send("Hi!")
 
 
-#  Create a Method for eg displayEmbed(parameters)
-#  Create a embed template so that we can call this method and pass in some information
-#  and it will post an embed with the following information
-#  OH yeah I did not mention it but can you make @ feature so that when recieving the Reminder the user gets notified
-#  K thanks :D
-
-async def displayEmbed(ctx,title,desc,colour):
+async def displayEmbed(ctx, title, desc, colour):
     await sleep(2)
-    main_embed=discord.Embed(title=title,description=desc,colour=colour)
+    main_embed = discord.Embed(
+        title=title,
+        description=desc,
+        colour=colour)
     await sleep(2)
     return main_embed
 
+
 @client.command()
 async def reminder(ctx, task, timings):
+    user = ctx.author.mention
     remindTime = 0
     now = datetime.now().strftime("%H%M%S")
     hour = int(now[0:2])
@@ -39,10 +39,10 @@ async def reminder(ctx, task, timings):
     sec = int(now[4:6])
     localTime = (hour*3600)+(min*60)+sec
 
-    title="Your Reminder"
-    description=task
-    colour=discord.Colour.dark_orange()
-    branch_embed=await displayEmbed(ctx,title,description,colour)
+    title = "Your Reminder"
+    description = task
+    colour = discord.Colour.dark_orange()
+    branch_embed = await displayEmbed(ctx, title, description, colour)
     while True:
         now = datetime.now().strftime("%H%M%S")
         hour = int(now[0:2])
@@ -59,6 +59,7 @@ async def reminder(ctx, task, timings):
             remindTime = localTime + (newtimings*3600)
 
         if currentTime == remindTime:
+            await ctx.send(user)
             await ctx.send(embed=branch_embed)
             break
 
