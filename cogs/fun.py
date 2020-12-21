a74cd6d6a3ffcb987bb2f4ba008f65f1c2c8d3ca
unknown <hajimeshanben45@gmail.com>
2020-12-22 02:41:17 +0530

    Added a dm when user joins, added 8ball function, and added anime search function

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from main import displayEmbed
 import wikipedia as wiki
+import random
+from mal.anime_search import AnimeSearch
 
 class Fun(commands.Cog):
 	def __init__(self, client):
@@ -26,6 +28,42 @@ class Fun(commands.Cog):
 		except wiki.DisambiguationError as e:
 			await ctx.send(e)
 
+	@commands.command(aliases=['8ball'])
+	async def eightball(self,ctx,string):
+		decision=random.randrange(0,2)
+		desc=f"Question asked: {string}"
+		color=0x000000
+		if(decision==0):
+			title="**NO!!!**"
+			no_embed=await displayEmbed(ctx,title,desc,color)
+			await ctx.send(embed=no_embed)
+		elif(decision==1):
+			title="**YES!!!**"
+			yes_embed=await displayEmbed(ctx,title,desc,color)
+			await ctx.send(embed=yes_embed)
+ 
+	@commands.command()
+	async def animesearch(self,ctx,name):
+			search=AnimeSearch(name,timeout=30)
+			color=0x000000
+			try:
+			
+				firstHit=search.results[0].title
+				synopsis=search.results[0].synopsis
+				episodes=search.results[0].episodes
+				score=search.results[0].score
+				img_url=search.results[0].image_url
+				url=search.results[0].url
+				anime_embed=await displayEmbed(ctx,firstHit,synopsis,color)
+				anime_embed.add_field(name="Number of episodes:",value=episodes,inline=True)
+				anime_embed.add_field(name="Score:",value=score,inline=True)
+				anime_embed.set_thumbnail(url=img_url)
+				anime_embed.set_footer(text=url)
+				
+				await ctx.send(embed=anime_embed)
+			except TimeoutError:
+				await ctx.send(ctx.author.mention)
+				await ctx.send("Try to reply within 30 seconds")
+
 def setup(client):
 	client.add_cog(Fun(client))
-
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from asyncio import sleep
 import os
+import random
 
 botToken = open("botToken.txt", "r")
 botToken = botToken.read()
@@ -10,6 +11,17 @@ client = commands.Bot(command_prefix="!bot ")
 @client.event
 async def on_ready():
     print("Mother file is online")
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    choice=random.randrange(1,3)
+    if choice==1:
+        await member.dm_channel.send(f'Hello! {member.name}, welcome to my Discord server!')
+    if choice==2:
+        await member.dm_channel.send(f"Hope you have a great time in our server")
+    if choice==3:
+        await member.dm_channel.send(f"WELCOME~~~~ {member.name}, this is a bot message to welcome you!")
 
 async def displayEmbed(ctx,title,desc,colour):
     await sleep(2)
